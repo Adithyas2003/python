684e2e25a65e814a948acfe708d53f92142a34e1
Adithyas2003 <adathyas49@gmail.com>
2024-06-15 00:49:25 -0700

    add new file

---
+++ new.py
@@ -0,0 +1,8 @@
+name=(input('enter your name'))
+roll_no=(input('enter your roll_no'))
+age=int(input('enter your age'))
+weight=float(input('enter your weight'))
+print(name,'name')
+print(roll_no,'roll_no')
+print(age,'age')
+print(weight,'weight')  
