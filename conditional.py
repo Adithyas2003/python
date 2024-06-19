942686df58b4325bb6e4421cdaed04b140dc46e6
Adithyas2003 <adathyas49@gmail.com>
2024-06-19 11:32:02 +0530

    new file added

---
+++ conditional.py
@@ -0,0 +1,6 @@
+a=int(input('Enter The Number:'))
+b=int(input('Enter The Number:'))
+if a>b:
+    print(a)
+else:
+    print(b)
---
+++ voting.py
@@ -0,0 +1,5 @@
+a=int(input("Enter The Number"))
+if 18<=a:
+    print("Eligible for voting")
+else:
+    print("Not Eligible For Voting")
