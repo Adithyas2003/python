bce052d495854f6ce0a453c28992c067feea83d2
Adithyas2003 <adathyas49@gmail.com>
2024-06-20 11:33:09 +0530

    new file added

---
+++ forloop.py
@@ -0,0 +1,37 @@
+"""for i in range(10):
+    print(i)"""
+"""for i in range(2,11):
+    print(i)"""
+"""for i in range(3,11,5):
+    print(i)"""
+"""a=int(input("Enter starting Number"))
+b=int(input("Enter Ending Number"))
+sum=0
+for i in range(a,b+1):
+    sum=sum+i
+print(sum)"""
+"""a=int(input("Enter starting Number"))
+b=int(input("Enter Ending Number"))
+sum=0
+for i in range(a,b+1):
+    if i%2==0:
+        sum=sum+i
+print(sum)"""
+
+    
+"""a=int(input("Enter starting Number"))
+b=int(input("Enter Ending Number"))
+sum=0
+for i in range(a,b+1):
+    if i%2==1:
+        sum=sum+i
+print(sum)"""
+
+a=int(input("enter Number"))
+fact=1
+for i in range(1,a+1):
+    fact=fact*i
+print(fact)
+
+
+
