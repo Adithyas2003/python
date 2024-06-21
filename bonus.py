2dcfcbff164344504c36ba0766fe1c16897919be
Adithyas2003 <adathyas49@gmail.com>
2024-06-21 11:18:43 +0530

    new file added

---
+++ bonus.py
@@ -0,0 +1,7 @@
+a=int(input("enter the salary"))
+b=int(input("enter years"))
+if b>=5:
+    total=a*0.05
+    print("bonus",total)
+else:
+    print("no bonus")
---
+++ string.py
@@ -0,0 +1,8 @@
+"""a="welcome"
+for i in a:
+    print(i)"""
+a="welcome"
+rev=""
+for i in a:
+  rev=i+rev
+print(rev)
