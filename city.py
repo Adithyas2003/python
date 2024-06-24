5ed44f921f9fc543edc3fb09c66c69d3bd8c4ac2
Adithyas2003 <adathyas49@gmail.com>
2024-06-24 11:23:10 +0530

    new file added

---
+++ city.py
@@ -0,0 +1,9 @@
+city=str(input("Enter city"))
+if city.upper()=="DELHI":
+    print("red fort")
+elif city.upper()=="AGRA":
+    print("tajmahal")
+elif city.upper()=="JAIPUR":
+     print("jalmahal")
+else:
+    print("enter valid city")
