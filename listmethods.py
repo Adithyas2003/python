305c5c727d4ea91c8d19f2732b5732c104d5f0df
Adithyas2003 <adathyas49@gmail.com>
2024-06-26 11:22:28 +0530

    new file added

---
+++ listmethods.py
@@ -0,0 +1,28 @@
+# L=[1,2,3]
+# L.append(10)
+# L.append([10,11])
+# L.insert(1,10)
+# L.extend([100,200,300])
+# print(L)
+# L=[1,2,3,'abc']
+# L.remove(3)
+# print(L)
+# L.pop()
+# print(L)
+# L.pop(1)
+# print(L)
+# L.clear()
+# print(L)
+# l=[1,2l=[1,2,3]
+# l.sort()
+# print(l),3,'abc']
+# print(l.index(1))
+# print(l.count(2))
+# l.reverse()
+# print(l)
+# l=[1,2,3]
+# l.sort()
+# print(l)
+
+
+
---
+++ students.py
@@ -0,0 +1,2 @@
+a=input("Enter how many students:")
+
