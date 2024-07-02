6c2e441b48215441be7e0863c839c0522dc3fb93
Adithyas2003 <adathyas49@gmail.com>
2024-07-02 11:27:05 +0530

    new file added

--- dictionary/numbers.py
+++ dictionary/numbers.py
@@ -1,3 +1,9 @@
 num={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
-a=int(input("enter the nuber:"))
-for i in num:
+a=int(input("enter the number:"))
+result=''
+while a>0:
+    d=a%10
+    word=num[d]
+    result=word+''+result
+    a=a//10
+    print(result)
---
+++ function.py
@@ -0,0 +1,10 @@
+# print('start')
+# def fun1():
+#     print('welcome')
+# fun1()
+# print('after')
+print('welcome')
+def fun1():
+    print('start')
+fun1()
+print('after')
