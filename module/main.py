4238332dfb4e82a5a6336a5a97f4983d783ce5f5
Adithyas2003 <adathyas49@gmail.com>
2024-07-06 11:04:58 +0530

    new file added

--- module/main.py
+++ module/main.py
@@ -1,6 +1,24 @@
+
 from reg import *
-data=[]
+from display import *
+from update import *
+from delete import *
+data=[{"id": "1", "name": "anu", "age": "22", "place": "kottayam"},
+    {"id": "2", "name": "krish", "age": "23", "place": "alappuzha"},]
 while True:
-    print("1.add \n 2.display \n 3.update \n 4.delete \n 5.exit \n")
-    ch=int(input("enter ur choice"))
-    
+    print("1.Add \n2.Display \n3.Update \n4.Delete \n5.Exit")
+    ch=int(input("Enter your choice : "))
+    if ch == 1:
+        register(data)
+    elif ch == 2:
+        display(data) 
+    elif ch == 3:
+        update(data)
+    elif ch == 4:
+        delete(data)
+    elif ch == 5:
+        print("You had exited")
+        break
+    else:
+        ("Invalid input")
+
