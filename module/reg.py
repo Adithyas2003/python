72613e1fd0519cfbe555b724335f83d1f41ae2d2
Adithyas2003 <adathyas49@gmail.com>
2024-07-06 10:10:12 +0530

    new file added

---
+++ module/reg.py
@@ -0,0 +1,6 @@
+def register(data):
+    name=input("enter the name")
+    age=int(input("enter the age"))
+    id=input("enter the id")
+    place=input("enter the place")
+    data={name,age,id,place}
