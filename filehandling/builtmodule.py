46c1c1bb2d38b4c0132627a31e003555fc099a3a
Adithyas2003 <adathyas49@gmail.com>
2024-07-10 11:22:06 +0530

    new file added

--- filehandling/builtmodule.py
+++ filehandling/builtmodule.py
@@ -18,11 +18,10 @@
 
 # f=open('python/filehandling/newpgrm.txt','w')
 # f.write("hello")
-
+a=int(input("Enter how many students:"))
 f=open('python/filehandling/listnew.txt','w')
 name=[]
-a=int(input("Enter how many students:"))
 for i in range(a):
     b=input('name:')
-    name.append(w)
-print(name)
+    name.append(a)
+    f.write(b)
