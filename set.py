13100c9a1546812a65e21317f2661e4eeefed5a5
Adithyas2003 <adathyas49@gmail.com>
2024-07-11 11:28:16 +0530

    new file added

--- set.py
+++ set.py
@@ -1,10 +1,44 @@
-s={1,2,3,4,5,8,9,1}
+# s={1,2,3,4,5,8,9,1}
 # print(s)
 # s1=set()
 # s2={}
 # for i in s:
 #      print(i)
-if 3 in s:
-    print('yes')
-else:
-    print('no')
+# if 3 in s:
+#     print('yes')
+# else:
+#     print('no')
+
+# x={'apple','orange','grape'}
+# y={'google','microsoft','grape'}
+# z=x.difference(y)
+# print(z)
+
+
+# x={'apple','orange','grape'}
+# y={'google','microsoft','grape'}
+# x.difference_update(y)
+# print(x)
+
+
+# x={'apple','orange','grape'}
+# y={'google','microsoft','grape'}
+# z=x.intersection(y)
+# print(z)
+
+# x = {"a", "b", "c"}
+# y = {"f", "e", "d", "c", "b", "a"}
+# z = x.issubset(y)
+# print(z)
+
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+# z = x.union(y)
+# print(z)
+
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple"}
+# x.intersection_update(y)
+# print(x)
+
+
