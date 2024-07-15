d7d3420238e264f3e1cad779619b4ed59335ec76
Adithyas2003 <adathyas49@gmail.com>
2024-07-15 11:29:46 +0530

    new file added

---
+++ oops/inheritance.py
@@ -0,0 +1,138 @@
+# class synnefo:
+#     def __init__(s):
+#         print("register")
+#     def python(s):
+#         print("python")
+#     def php(self):
+#         print("php")
+
+
+# class novavi(synnefo):
+#     def dm(self):
+#         print("dm")
+#     def web_dev(self):
+#         print("web_dev")
+# manu=synnefo()
+# manu.python()
+# sanu=novavi()
+# sanu.php()
+
+
+# class synnefo:
+#     def __init__(s):
+#         print("register")
+#     def python(s):
+#         print("python")
+#     def php(self):
+
+#     def dm(self):
+#         print("dm")
+#     def web_dev(self):
+#         print("web_dev")
+# class student(novavi,synnefo):
+#     def reg(self):
+#         print("reg")
+# manu=synnefo()
+# manu.python()
+# sanu=novavi()
+# sanu.web_dev()
+# anu=student()
+# anu.reg()
+
+#multilevel inheritance:
+
+
+# class synnefo:
+#     def __init__(s):
+#         print("register")
+#       def__init__(s)def python(s):
+#         print("python")
+#     def php(self):
+#         print("php")
+# class novavi:
+#     def dm(self):
+#         print("dm")
+#     def web_dev(self):
+#         print("web_dev")
+# class staff(novavi):
+#     def reg(self):
+#         print("reg")
+# manu=synnefo()
+# manu.python()
+# sanu=novavi()
+# sanu.web_dev()
+# anu=staff()
+# anu.reg()
+
+#hierarchial inheritance:
+#   def__init__(s)
+
+# class synnefo:
+#     def __init__(s):
+#         print("register")
+#     def python(s):
+#         print("python")
+#     def php(self):
+#         print("php")
+# class novavi:
+#     def dm(self):
+#         print("dm")
+#     def web_dev(self):
+#         print("web_dev")
+# class student(synnefo):
+#     def reg(self):
+#         print("reg")
+# manu=synnefo()
+# manu.python()
+# sanu=novavi()
+# sanu.web_dev()
+# anu=student()
+# anu.reg()
+
+
+#hybrid inheritance:
+
+# class A:
+#     def a1(self):
+#         print("a1")
+
+# class B:
+#     def b1(self):
+#         print("b1")
+# class F:
+#     def f1(self):
+#         print("f1")
+# class C(A,B):
+#     def c1(self):
+#         print("c1")
+# class D(B,F):
+#     def d1(self):
+#         print("d1")
+# class E(C,D):
+#     def e1(self):
+#         print("e1")
+# abc=A()
+# abc.a1()
+# hij=B()
+# hij.b1()
+# klm=C()
+# klm.c1()
+# mno=D()
+# mno.d1()
+# pqr=E()
+# pqr.e1()
+# xyz=F()
+# xyz.f1()
+
+
+#example program:
+class Birds:
+    def __init__(self,species):
+        self.species=species
+
+
+
+
+
+
+
