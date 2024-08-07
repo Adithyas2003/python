1a92fc73d52ed47246f6973b078534c4b1c37e31
Adithyas2003 <adathyas49@gmail.com>
2024-08-07 19:27:10 +0530

    new file add

---
+++ prgm.py
@@ -0,0 +1,16 @@
+#work 1:
+
+# rows=[5,4,6,3,2,5,1]
+# for i in rows:
+#     print("*" * i)
+
+
+#work2:
+
+# n=5
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print("",end="")
+#     for k in range(i+1):
+#         print("*",end="")
+#     print()
