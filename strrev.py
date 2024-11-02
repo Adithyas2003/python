235d27bf3cd3161984a4657c2f753fe9fa82fa92
Adithyas2003 <adathyas49@gmail.com>
2024-11-02 11:24:49 +0530

    string reverse

---
+++ strrev.py
@@ -0,0 +1,70 @@
+
+# reverseofstring:
+
+
+# a='hello'
+# d=''
+# i=len(a)-1
+# while i>=0:
+#     d+=a[i]
+#     i-=1
+# print(d)
+
+
+
+
+
+
+
+# largeststring:
+
+# l=['abc','hjkl']
+# a=''
+# d=0
+# for i in l:
+#     if len(i)>d:
+#         d=len(i)
+#         a=i
+# print(a)
+
+
+
+
+# largest number:
+
+# l=[1,22,3,4,5,6]
+# a=l[0]
+# for i in l:
+#     if i>a:
+#         a=i
+# print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
